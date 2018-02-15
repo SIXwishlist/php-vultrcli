90b22656a14e2215541423d4b8f40ac2f5f76707
Tajul Asri <mtajulasri@gmail.com>
2018-02-15 11:42:22 +0800

    render os and plan list into table

--- src/Commands/OsListCommand.php
+++ src/Commands/OsListCommand.php
@@ -9,6 +9,7 @@
 namespace Vultr\Commands;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -24,9 +25,32 @@ class OsListCommand extends Command
             ->setHelp('If the "windows" flag is true, a Windows license will be included with the instance, which will increase the cost.');
     }
 
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $run = (new OperatingSystem())->oses();
-        $output->write($run);
+
+        $tableData = [];
+
+        foreach (json_decode($run) as $osData) {
+            $tableData[] = [
+                $osData->OSID,
+                $osData->name,
+                $osData->arch,
+                $osData->family,
+            ];
+        }
+
+        $table = new Table($output);
+
+        $table
+            ->setHeaders([
+                'ID', 'Name', 'Architecture', 'Family',
+            ])
+            ->setRows($tableData)
+            ->render();
     }
 }
--- src/Commands/PlansListCommand.php
+++ src/Commands/PlansListCommand.php
@@ -3,6 +3,7 @@
 namespace Vultr\Commands;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -18,9 +19,35 @@ class PlansListCommand extends Command
             ->addArgument('type', InputArgument::REQUIRED, 'instances type integer');
     }
 
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $plans = (new Plans())->servers($input->getArgument('type'));
-        $output->write($plans);
+        $plans = (new Plans())->plans($input->getArgument('type'));
+
+        $table = new Table($output);
+        $tableData = [];
+
+        foreach (json_decode($plans) as $plan) {
+
+            $tableData[] = [
+                $plan->VPSPLANID,
+                $plan->name,
+                $plan->vcpu_count,
+                $plan->ram,
+                $plan->disk,
+                $plan->bandwidth,
+                $plan->price_per_month,
+            ];
+        }
+
+        $table
+            ->setHeaders([
+                'PLAN ID', 'Name', 'Cpu count', 'RAM', 'Disk Size', 'Bandwidth', 'Price (USD)',
+            ])
+            ->setRows($tableData)
+            ->render();
     }
 }
